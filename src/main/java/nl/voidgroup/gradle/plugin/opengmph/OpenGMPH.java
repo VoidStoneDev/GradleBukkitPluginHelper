154585425c1a8284e94ddc207072a23e5cc0d3c9
VoidStone <janpieter.stolk@kpnmail.nl>
2021-03-20 10:54:48 +0100

    Supressed unused on OpenGMPH class

--- src/main/java/nl/voidgroup/gradle/plugin/opengmph/OpenGMPH.java
+++ src/main/java/nl/voidgroup/gradle/plugin/opengmph/OpenGMPH.java
@@ -2,7 +2,7 @@ package nl.voidgroup.gradle.plugin.opengmph;
 
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
-
+@SuppressWarnings("unused")
 public class OpenGMPH implements Plugin<Project> {
     @Override
     public void apply(Project project) {
