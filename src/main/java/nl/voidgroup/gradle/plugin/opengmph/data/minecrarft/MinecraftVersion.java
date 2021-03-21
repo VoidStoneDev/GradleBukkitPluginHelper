0da87026b14cdd64c8fb8004da947a2a2a351f16
VoidStone <janpieter.stolk@kpnmail.nl>
2021-03-21 15:31:45 +0100

    Added MinecraftVersion enum

---
+++ src/main/java/nl/voidgroup/gradle/plugin/opengmph/data/minecrarft/MinecraftVersion.java
@@ -0,0 +1,13 @@
+package nl.voidgroup.gradle.plugin.opengmph.data.minecrarft;
+
+public enum MinecraftVersion {
+    V1_8,
+    V1_9,
+    V1_10,
+    V1_11,
+    V1_12,
+    V1_13,
+    V1_14,
+    V1_15,
+    V1_16
+}
