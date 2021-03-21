3b229dd00a7c66eb53f94491c437f63fcf5b50cf
VoidStone <janpieter.stolk@kpnmail.nl>
2021-03-21 19:11:13 +0100

    Clean

--- src/main/java/nl/voidgroup/gradle/plugin/opengmph/OpenGMPH.java
+++ src/main/java/nl/voidgroup/gradle/plugin/opengmph/OpenGMPH.java
@@ -3,7 +3,11 @@ package nl.voidgroup.gradle.plugin.opengmph;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 
+import java.net.URI;
+
 public class OpenGMPH implements Plugin<Project> {
+
+
     @Override
     public void apply(Project project) {
 
--- src/main/java/nl/voidgroup/gradle/plugin/opengmph/extension/PluginData.java
+++
@@ -1,8 +0,0 @@
-package nl.voidgroup.gradle.plugin.opengmph.extension;
-
-import nl.voidgroup.gradle.plugin.opengmph.data.minecrarft.MinecraftServer;
-
-public class PluginData {
-    public MinecraftServer targetServer;
-    public boolean importRepo = true;
-}
