a5ea83490ca12cc01cd022ee3b49a27580bbd0f8
VoidStone <janpieter.stolk@kpnmail.nl>
2021-03-22 15:38:49 +0100

    Added Util class

---
+++ src/main/java/nl/voidgroup/gradle/plugin/opengmph/Util.java
@@ -0,0 +1,15 @@
+package nl.voidgroup.gradle.plugin.opengmph;
+
+public class Util {
+
+    public static <T> T requireNonNull(T obj, RuntimeException ex) {
+        if(obj == null) throw ex;
+        return obj;
+    }
+    public static <T> T requireNonNull(T obj) {
+        return requireNonNull(obj, "Object cannot be null");
+    }
+    public static <T> T requireNonNull(T obj, String msg) {
+        return requireNonNull(obj, new NullPointerException(msg));
+    }
+}
