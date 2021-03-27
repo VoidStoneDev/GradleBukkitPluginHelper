b5e5812ec6c347ea9eef15297a7fc0098e1e1e20
VoidStone <janpieter.stolk@kpnmail.nl>
2021-03-27 20:53:35 +0100

    Added 1.16 api version to SpigotServer

---
+++ src/main/java/nl/voidgroup/gradle/plugin/opengmph/data/spigot/SpigotServer.java
@@ -0,0 +1,20 @@
+package nl.voidgroup.gradle.plugin.opengmph.data.spigot;
+
+import nl.voidgroup.gradle.plugin.opengmph.annotation.Repository;
+import nl.voidgroup.gradle.plugin.opengmph.annotation.ServerAPI;
+import nl.voidgroup.gradle.plugin.opengmph.data.minecraft.MinecraftServer;
+import nl.voidgroup.gradle.plugin.opengmph.data.minecraft.MinecraftVersion;
+
+import java.util.HashMap;
+import java.util.Map;
+@ServerAPI(group = "org.spigotmc", id = "spigot-api")
+@Repository(name = "spigot-repo", url = "https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+public class SpigotServer extends MinecraftServer {
+
+    @Override
+    public Map<MinecraftVersion, String> getAPIVersions() {
+        return new HashMap<MinecraftVersion, String>() {{
+            put(MinecraftVersion.V1_16, "1.16.5-R0.1-SNAPSHOT");
+        }};
+    }
+}
